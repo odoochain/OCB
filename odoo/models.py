57cb0aaf2a7011f86d1cfcc3f437bacab65d2268
Vincent <wjs_net@163.com>
2019-05-23 10:36:43 +0800

    update the method read(), if contains field 'tx_id', check the transaction is right or not

--- odoo/models.py
+++ odoo/models.py
@@ -53,7 +53,7 @@ from .exceptions import AccessError, MissingError, ValidationError, UserError
 from .osv.query import Query
 from .tools import frozendict, lazy_classproperty, lazy_property, ormcache, \
                    Collector, LastOrderedSet, OrderedSet, pycompat, groupby
-# from odoo.tools import octa_bdb_api
+from odoo.tools import octa_bdb_api
 from .tools.config import config
 from .tools.func import frame_codeinfo
 from .tools.misc import CountingStream, clean_context, DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
@@ -2791,13 +2791,13 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
                             vals[name] = "ffFalse"
                         else:
                             try:
-                                # query_data = octa_bdb_api.query_transaction_by_id(tx_id, bdb_host=config.options['octa-chain-host'],
-                                #                                  bdb_port=int(config.options['octa-chain-port']))
-                                # _logger.info(query_data)
-                                vals[name] = "tttrue"
+                                query_data = octa_bdb_api.query_transaction_by_id(tx_id, bdb_host=config.options['octa-chain-host'],
+                                                                 bdb_port=int(config.options['octa-chain-port']))
+                                _logger.debug(query_data)
+                                vals[name] = "True"
                             except Exception as e:  # 如果发现错误，返回前端，数据不安全
                                 _logger.error(e)
-                                vals[name] = "fffalse"
+                                vals[name] = "False"
                 except MissingError:
                     vals.clear()
         result = [vals for record, vals in data if vals]
