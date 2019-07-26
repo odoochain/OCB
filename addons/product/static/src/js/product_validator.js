36ee44ad61929eefd24af391d92d0b07a16955d1
Evelynzzz <15104026362@163.com>
2019-07-26 12:42:59 +0800

    Add tx_id field and define a validator

---
+++ addons/product/static/src/js/product_validator.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Evelyn ZHONG
+ */
+odoo.define('product.validator', function (require) {
+    "use strict";
+
+    var fieldRegistry = require('web.field_registry');
+    var basicFields = require('web.basic_fields');
+
+    var validator = basicFields.FieldText.extend({
+        /**
+         * @constructor
+         * @override
+         */
+        init: function(parent, name, record, options) {
+            this._super(parent, name, record, options);
+            var tx_id = record.data.tx_id
+            if(tx_id === 'False'){
+                this.do_warn('区块链验证错误！');
+            }
+        }
+    })
+
+    fieldRegistry.add('product_validator', validator);
+})
