bb50b5755e5c093681b4abc42b96eb3d0fa13c9f
imtoohard <info@odoochain.com>
2023-08-27 07:46:43 +0800

    参考此文替换国内愿 https://blog.csdn.net/klsfp/article/details/118228645

--- addons/website/models/assets.py
+++ addons/website/models/assets.py
@@ -82,7 +82,7 @@ class Assets(models.AbstractModel):
                         statement = src.group()
                         url, font_format = re.match(r'src: url\(([^\)]+)\) (.+)', statement).groups()
                         req = requests.get(url, timeout=5, headers=headers_woff2)
-                        # https://fonts.gstatic.com/s/modak/v18/EJRYQgs1XtIEskMB-hRp7w.woff2
+                        # https://gapis.geekzu.org/g-fonts/s/modak/v18/EJRYQgs1XtIEskMB-hRp7w.woff2
                         # -> s-modak-v18-EJRYQgs1XtIEskMB-hRp7w.woff2
                         name = url_parse(url).path.lstrip('/').replace('/', '-')
                         attachment = IrAttachment.create({
