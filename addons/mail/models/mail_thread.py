bfbdd747ecee856b6f45d1b996b9f34875ea3a44
imtoohard <info@odoochain.com>
2023-08-27 17:47:24 +0800

    fix mail_thread.py

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -106,7 +106,7 @@ class MailThread(models.AbstractModel):
     message_has_error_counter = fields.Integer(
         'Number of errors', compute='_compute_message_has_error',
         help="Number of messages with delivery error")
-    message_attachment_count = fields.Integer('Attachment Count', compute='_compute_message_attachment_count', groups="base.group_user")
+    message_attachment_count = fields.Integer('Message Attachment Count', compute='_compute_message_attachment_count', groups="base.group_user")
     message_main_attachment_id = fields.Many2one(string="Main Attachment", comodel_name='ir.attachment', copy=False)
 
     @api.depends('message_follower_ids')
